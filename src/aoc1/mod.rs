8168809cdbd263f1e98b4675cfbb07b98a286fe9
lamHoussam <houssam.lamlih@hotmail.com>
2023-12-06 12:24:59 +0100

    Refactor AOC1 day 1

--- src/aoc1/mod.rs
+++ src/aoc1/mod.rs
@@ -1,52 +1,30 @@
 use std::fs::File;
 use std::io::{self, BufRead};
 
-pub fn get_code_digits(msg: String) -> i32 {
-    let mut frst: Option<char> = None;
-    let mut last: Option<char> = None;
+fn get_code_digits(msg: String) -> i32 {
+    let values: Vec<char> =  msg.chars().filter(|m| m.is_digit(10)).collect();
+    let value = if values.len() != 0 {
+        values[0].to_string() + &values[values.len() - 1].to_string()
+    } else {
+        "0".to_string()
+    };
 
-    for i in msg.chars() {
-        if i.is_digit(10) {
-            if frst == None {
-                frst = Some(i);
-                last = Some(i);
-            } else {
-                last = Some(i);
-            }
-        }
-    }
-
-    let value = frst.unwrap_or_default().to_string() + &last.unwrap_or_default().to_string(); 
     return value.parse::<i32>().unwrap_or_default();
 }
 
 pub fn get_code_sum(file_name: &str) -> Option<i32> {
-
-    let mut res: i32 = 0;
     let file = File::open(file_name);
 
-    match file {
+    return match file {
         Ok(f) => {
             let reader = io::BufReader::new(f);
-
-            for line in reader.lines() {
-                match line {
-                    Ok(content) => {
-                        res += get_code_digits(content);
-                    },
-                    Err(_) => {
-                        println!("Couldnt open file {}", file_name);
-                        return None;
-                    },
-                }
-            }
+            Some(reader.lines()
+                .map(|content| get_code_digits(content.unwrap_or_default()))
+                .into_iter().sum()) 
         },
         Err(_) => {
             println!("Couldnt open file {}", file_name);
             return None;
         },
-    } 
-
-
-    return Some(res);
+    }; 
 }
