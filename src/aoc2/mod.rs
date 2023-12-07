b74521ce5701ae1c1f0e94f25e4cc5ab3dec1cea
lamHoussam <houssam.lamlih@hotmail.com>
2023-12-07 17:06:11 +0100

    AOC Day 2 Part 2

--- src/aoc2/mod.rs
+++ src/aoc2/mod.rs
@@ -19,14 +19,44 @@ fn check_number_cubes(number: i32, color: &str) -> bool {
     }
 }
 
-pub fn get_correct_ids_sum(file_name: &str) -> Option<i32> {
+
+fn get_power_set_for_game(msg: String) -> i32 {
+
+    let mut max_colors = (0, 0, 0);
+
+    let pattern = r"Game (?P<id>\d+): (?P<sequence>(\d+ (red|blue|green)(,|;) )*\d+ (red|blue|green))";
+    let regexp = Regex::new(pattern).unwrap();
+
+    if let Some(captures) = regexp.captures(msg.as_str()) {
+        if let Some(sequence) = captures.name("sequence") {
+            let values_colors: Vec<(&str, &str)> = sequence.as_str()
+                .split(|c| c == ',' || c == ';').map(|s| {
+                    let mut parts = s.trim().splitn(2, ' ');
+                    (parts.next().unwrap(), parts.next().unwrap())
+                }).collect();
+            
+            for (v, c) in values_colors {
+                let value = v.parse::<i32>().unwrap_or(1);
+                match c {
+                    "red" => if value > max_colors.0 { max_colors.0 = value; }
+                    "green" => if value > max_colors.1 { max_colors.1 = value; }
+                    "blue" => if value > max_colors.2 { max_colors.2 = value; }
+                    _ => (),
+                }                
+            }
+        }
+    }
+    return max_colors.0 * max_colors.1 * max_colors.2;
+} 
+
+pub fn get_power_set_sum(file_name: &str) -> Option<i32> {
     let file = File::open(file_name);
 
     return match file {
         Ok(f) => {
             let reader = io::BufReader::new(f);
             Some(reader.lines()
-                .map(|content| parse_line(content.unwrap_or_default()))
+                .map(|content| get_power_set_for_game(content.unwrap_or_default()))
                 .into_iter().sum()) 
         },
         Err(_) => {
@@ -36,7 +66,7 @@ pub fn get_correct_ids_sum(file_name: &str) -> Option<i32> {
     };
 }
 
-fn parse_line(msg: String) -> i32 {
+fn get_game_id_if_possible(msg: String) -> i32 {
     let pattern = r"Game (?P<id>\d+): (?P<sequence>(\d+ (red|blue|green)(,|;) )*\d+ (red|blue|green))";
     let regexp = Regex::new(pattern).unwrap();
 
@@ -65,4 +95,20 @@ fn parse_line(msg: String) -> i32 {
 
 }
 
+pub fn get_correct_ids_sum(file_name: &str) -> Option<i32> {
+    let file = File::open(file_name);
+
+    return match file {
+        Ok(f) => {
+            let reader = io::BufReader::new(f);
+            Some(reader.lines()
+                .map(|content| get_game_id_if_possible(content.unwrap_or_default()))
+                .into_iter().sum()) 
+        },
+        Err(_) => {
+            println!("Couldnt open file {}", file_name);
+            return None;
+        },
+    };
+}
 
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,30 @@
-use crate::aoc1::*;
-use crate::aoc2::*;
 
 mod aoc2;
 mod aoc1;
 
 
 fn main() {
-    // let v1 = get_code_sum("src/aoc1/input.txt"); 
-    // println!("AOC1: {}", v1.unwrap_or(-1));
+/*
+    /// Day 1
+    let v1 = gcrate::aoc1::et_code_sum("src/aoc1/input.txt"); 
+    println!("AOC1: {}", v1.unwrap_or(-1));
 
-    // let v2 = get_code_sum2("src/aoc1/input.txt"); 
-    // println!("AOC2: {}", v2.unwrap_or(-1));
+    /// Part 2
+    let v2 = crate::aoc1::get_code_sum2("src/aoc1/input.txt"); 
+    println!("AOC2: {}", v2.unwrap_or(-1));
 
-    let v = get_correct_ids_sum("src/aoc2/input.txt");
+*/
+
+/*
+    ///Day 2
+    let v = crate::aoc2::get_correct_ids_sum("src/aoc2/input.txt");
     println!("{}", v.unwrap_or_default());
+
+    /// Part2
+    
+    */
+    
+    let v = crate::aoc2::get_power_set_sum("src/aoc2/input.txt");
+    println!("{}", v.unwrap_or_default());
+
 }
